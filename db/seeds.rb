6ce83ce945da999f6e1f70618661a03fe7d53ae4
Alexandre Mathieu <alexandre.mathieu1000@gmail.com>
2018-02-14 12:30:19 +0100

    db and models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     'italian',
+
+  },
+  {
+    name:         'Esprit Tchai',
+    address:      '13 rue Oberkampf',
+    category:      'italian',
+
+  },
+  {
+    name:         'Chez Maxou',
+    address:      'Rue de la raclette',
+    category:      "french",
+
+  },
+  {
+    name:         'A la galette Bretonne',
+    address:      '2 avenue de concarneau, Concarneau',
+    category:      "french",
+
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
